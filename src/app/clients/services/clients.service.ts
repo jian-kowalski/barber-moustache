f4895f9fcbe2142f5fb9144658544eea5ab7c437
jian <jian_kowalski@hotmail.com>
2022-07-12 21:45:45 -0300

    Criação do Crud generico

--- src/app/clients/services/clients.service.ts
+++ src/app/clients/services/clients.service.ts
@@ -1,44 +1,17 @@
-import { environment } from './../../../environments/environment.prod';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, first } from 'rxjs';
 
+import { CrudService } from './../../shared/crud/crud.service';
 import { Client } from './../model/client';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ClientsService {
-  private readonly API = `${environment.API}clients`;
+export class ClientsService extends CrudService<Client> {
 
-  constructor(private httpClient: HttpClient) {}
-
-  list() {
-    return this.httpClient.get<Client[]>(this.API).pipe(first());
+  constructor(protected override httpClient: HttpClient) {
+    super(httpClient, "clients");
   }
 
-  private create(client: Client) {
-    return this.httpClient.post<Client>(this.API, client).pipe(first());
-  }
 
-  loadById(id: string) {
-    return this.httpClient.get<Client>(`${this.API}/${id}`).pipe(first());
-  }
-
-  delete(id: string) {
-    return this.httpClient.delete(`${this.API}/${id}`).pipe(first());
-  }
-
-  private update(client: Client) {
-    return this.httpClient
-      .put<Client>(`${this.API}/${client.id}`, client)
-      .pipe(first());
-  }
-
-  save(client: Client) {
-    if (client.id) {
-      return this.update(client);
-    }
-    return this.create(client);
-  }
 }
---
+++ src/app/shared/crud/crud.service.ts
@@ -0,0 +1,47 @@
+import { environment } from './../../../environments/environment.prod';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { first } from 'rxjs';
+import { Crud } from './crud';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CrudService<T extends Crud> {
+  private readonly API = `${environment.API}`;
+
+  constructor(protected httpClient: HttpClient, @Inject('API_URI') private API_URI: string) {}
+
+  protected getApi(): string {
+    return `${this.API}${this.API_URI}`;
+  }
+
+  list() {
+    return this.httpClient.get<T[]>(this.getApi()).pipe(first());
+  }
+
+  private create(record: T) {
+    return this.httpClient.post<T>(this.getApi(), record).pipe(first());
+  }
+
+  loadById(id: string) {
+    return this.httpClient.get<T>(`${this.getApi()}/${id}`).pipe(first());
+  }
+
+  delete(id: string) {
+    return this.httpClient.delete(`${this.getApi()}/${id}`).pipe(first());
+  }
+
+  private update(record: T) {
+    return this.httpClient
+      .put<T>(`${this.getApi()}/${record.id}`, record)
+      .pipe(first());
+  }
+
+  save(record: T) {
+    if (record.id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+}
